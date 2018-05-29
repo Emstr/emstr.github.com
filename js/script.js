2d5fc0797c0a8fb6aec4fc0a14c71d74376d434b
Emory Fierlinger <emory.fierlinger@gmail.com>
2018-05-29 23:42:53 +1200

    Added safety section, fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-import ScrollReveal from 'scrollreveal'
+var pageLoad = 0
 
-window.sr = ScrollReveal({ reset: true });
-sr.reveal('.card', { duration: 200 });
+function testScroll(ev){
+    if(window.pageYOffset>200 && pageLoad==0) {
+        pageLoad +=
+        $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+    }
+}
+window.onscroll=testScroll
 
-console.log("yoza")
+
+
